a5121b9fec61684c046425c110e8f90cf8782302
gracerunge <rungegn@miamioh.edu>
2023-11-13 13:24:08 -0500

    small bug fixed

--- STA308_HW4.R
+++ STA308_HW4.R
@@ -18,14 +18,14 @@ roulette_streaks <- function(num_spins=600) {
   spin <- sample(c("Black", "Red", "Green"), size=num_spins, 
                  prob=c(18/38, 18/38, 2/38), replace = TRUE)
   number=1
-  streak_number = 0 
+  streak_number = 1 
   streak_list = list()
   while(number<length(spin)){
     if(spin[number] == spin[number + 1]){
       streak_number = streak_number + 1
     } else {
       streak_list <- c(streak_list, streak_number)
-      streak_number = 0 
+      streak_number = 1 
     } 
     number = number + 1
   }
@@ -44,16 +44,14 @@ summary(roulette_simulation_1)
 hist(roulette_simulation_1)
 
 #* The summary of this situation provides a minimum length of a 
-#*   streak as 4 and the maximum of 17. The mean is about 7.8
-#*   and the median is 8. This indicated to me even before 
-#*   running the histogram code that the distribution would be 
-#*   right skewed as the median is pulling the data in that 
-#*   direction. The histogram confirmed this assumption and shows 
-#*   a right skewed bell curve shape. If you are playing roulette 
+#*   streak as 5 and the maximum of 18. The mean is about 8.9
+#*   and the median is 9. The mean and median are pretty close 
+#*   so I expect the shape to be close to symmetrical. The histogram shows a 
+#*   slightly right skewed bell curve shape. If you are playing roulette 
 #*   in a casino you would be very lucky to get a maximum streak 
-#*   on the higher end of this distribution, but this shows it happens
-#*   occasionally and this is perhaps why people continue to play 
-#*   games like this for the small chance to win big.
+#*   on the higher end of this distribution, but this simulation shows it 
+#*   does happen occasionally and this is perhaps why people continue 
+#*   to play games like this for the small chance to win big.
 #* 
 
 #* Suppose a casino can do 1200 spins per day as 
@@ -68,19 +66,19 @@ hist(roulette_simulation_2)
 #* If a casino could increase the number of spins per day to 
 #*   1200 (double the standard of 600) the distribution of 
 #*   longest streaks is going to change at least slightly. 
-#*   The minimum is now 5 and the maximum is 19. The range of streaks 
+#*   The minimum is now 6 and the maximum is 20. The range of streaks 
 #*   increased in this new scenario. The range of streaks in this 
 #*   distribution is 14 which is 1 larger than the range of 13 in 
-#*   the previous simulation.
-#*   The median is still 8, but the mean has now increased from 
-#*   7.8 to 8.7. The more times you are able to run the simulation 
+#*   the previous simulation. The minimum streak has increased by 1 as well.
+#*   The median is still 9, but the mean has now increased from 
+#*   to 9.7. The more times you are able to run the simulation 
 #*   in a day the more likely you are to get a higher streak if 
 #*   your criteria is strictly the mean. 
 #*   The histogram still shows a right skewed distribution but the main
-#*   peak appears less spread out than the previous situation. 
+#*   peak appears slightly less spread out than the previous situation. 
 #*   The two distributions are not drastically different. 
-#*   I think you would have to change the number 
-#*   of spins or the number of repetitions even more to see more drastic 
+#*   I think you would have to change the number of spins
+#*   or the number of repetitions even more to see more drastic 
 #*   changes to the distribution.
 #*   
 
